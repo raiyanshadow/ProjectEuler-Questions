8df1925d1561b4898ed70503cec570552739cd93
raiyan <67802404+raiyanshadow@users.noreply.github.com>
2022-08-10 01:42:36 -0400

    Update and rename EulQ6.c to EulQ06.c

--- EulQ06.c
+++ EulQ06.c
@@ -1,15 +1,13 @@
 // Sum square difference
 
-#include <stdio.h>
-#include <time.h>
-#include <math.h>
+#include "essentials.c"
 
 int main()
 {
-    float timeStart = (float) clock () / CLOCKS_PER_SEC;
-
     int x;
     scanf("%i", &x);
+
+    timeStart();
 
     long long squareOfSum = pow((x * (x + 1)) / 2, 2);                // sum of n numbers formula then squared
     long long sumOfSquares = (x * (x + 1) * (2 * x + 1)) / 6;         // sum of n^2 numbers formula
@@ -17,6 +15,5 @@ int main()
 
     printf("%lli\n", diff);
 
-    float timeEnd = (float) clock () / CLOCKS_PER_SEC;
-    printf("Time elapsed: %f", timeEnd-timeStart);
+    timeEnd();
 }
