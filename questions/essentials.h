03abce24618d6c7e5ba21e16b9d01d1024923871
raiyan <67802404+raiyanshadow@users.noreply.github.com>
2022-08-13 13:08:08 -0400

    Update essentials.h

--- questions/essentials.h
+++ questions/essentials.h
@@ -4,9 +4,16 @@
 #include <string.h>
 #include <time.h>
 #include <stdbool.h>
+#include <limits.h>
+#include "bignum.c"
 
 #ifndef ESSENTIALS_H
 #define ESSENTIALS_H
+
+typedef struct orderedPair {
+    int x;
+    int y;
+} orderedPair;
 
 #define MAX(i1, i2) (i1 > i2 ? i1 : i2)
 
@@ -16,6 +23,9 @@ void timeStart();
 void timeEnd();
 int isPrime(long long x);
 int getFileLines(char *fname);
-int getLineLength(char *fname);
+int getLineLength(char *fname, char sep);
+orderedPair makePair(int x, int y);
+void choose(int x, int y, bignum* ret);
+void factorial(int x, bignum *ret);
 
 #endif
