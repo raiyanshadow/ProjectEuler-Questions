897ef444bff270fe6c2228cfb9e64aa23a5989c8
raiyan <67802404+raiyanshadow@users.noreply.github.com>
2022-08-13 22:33:33 -0400

    Update bignum.c

--- questions/bignum.c
+++ questions/bignum.c
@@ -1,42 +1,15 @@
-/*	bignum.c
-	Implementation of large integer arithmetic: addition, subtraction,
-		multiplication, and division.
-
-	begun: February 7, 2002
-	by: Steven Skiena
-*/
-
-/*
-Copyright 2003 by Steven S. Skiena; all rights reserved. 
-
-Permission is granted for use in non-commerical applications
-provided this copyright notice remains intact and unchanged.
-
-This program appears in my book:
-
-"Programming Challenges: The Programming Contest Training Manual"
-by Steven Skiena and Miguel Revilla, Springer-Verlag, New York 2003.
-
-See our website www.programming-challenges.com for additional information.
-
-This book can be ordered from Amazon.com at
-
-http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
-
-*/
-
-
 #include <stdio.h>
 
-#define	MAXDIGITS	1000		/* maximum length bignum */ 
+#define	MAXDIGITS	 1000			/* maximum length bignum */ 
 
-#define PLUS		1		/* positive sign bit */
-#define MINUS		-1		/* negative sign bit */
-#define MAX(i1, i2) (i1 > i2 ? i1 : i2)
+#define PLUS		 1			/* positive sign bit */
+#define MINUS		-1			/* negative sign bit */
+
+#define MAX(i1, i2) (i1 > i2 ? i1 : i2)		/* return larger value */
 
 typedef struct {
         char digits[MAXDIGITS];         /* represent the number */
-		int signbit;			/* 1 if positive, -1 if negative */ 
+	int signbit;			/* 1 if positive, -1 if negative */ 
         int lastdigit;			/* index of high-order digit */
 } bignum;
 
@@ -45,9 +18,15 @@ void print_bignum(bignum *n)
 {
 	int i;
 
-	if (n->signbit == MINUS) printf("- ");
-	for (i=n->lastdigit; i>=0; i--)
-		printf("%c",'0'+ n->digits[i]);
+	if (n->signbit == MINUS) 
+	{
+		printf("- ");
+	}
+	
+	for (i = n->lastdigit; i >= 0; i--)
+	{
+		printf("%c",'0' + n->digits[i]);
+	}
 
 	printf("\n");
 	return;
@@ -58,22 +37,36 @@ void int_to_bignum(int s, bignum *n)
 	int i;				/* counter */
 	int t;				/* int to work with */
 
-	if (s >= 0) n->signbit = PLUS;
-	else n->signbit = MINUS;
+	if (s >= 0)
+	{	
+		n->signbit = PLUS;
+	}
+	else
+	{
+		n->signbit = MINUS;
+	}
 
-	for (i=0; i<MAXDIGITS; i++) n->digits[i] = (char) 0;
+	for (i = 0; i < MAXDIGITS; i++)
+	{	
+		n->digits[i] = (char) 0;
+	}
 
 	n->lastdigit = -1;
 
 	t = abs(s);
 
-	while (t > 0) {
-		n->lastdigit ++;
-		n->digits[ n->lastdigit ] = (t % 10);
+	while (t > 0) 
+	{
+		n->lastdigit++;
+		n->digits[n->lastdigit] = (t % 10);
 		t = t / 10;
 	}
 
-	if (s == 0) n->lastdigit = 0;
+	if (s == 0)
+	{
+		n->lastdigit = 0;
+	}
+	
 	return;
 }
 
@@ -102,25 +95,34 @@ void add_bignum(bignum *a, bignum *b, bignum *c)
 
 	initialize_bignum(c);
 
-	if (a->signbit == b->signbit) c->signbit = a->signbit;
-	else {
-		if (a->signbit == MINUS) {
+	if (a->signbit == b->signbit)
+	{	
+		c->signbit = a->signbit;
+	}
+	else 
+	{
+		if (a->signbit == MINUS) 
+		{
 			a->signbit = PLUS;
-			subtract_bignum(b,a,c);
+			subtract_bignum(b, a, c);
 			a->signbit = MINUS;
-		} else {
+		} 
+		else 
+		{
                         b->signbit = PLUS;
-                        subtract_bignum(a,b,c);
+                        subtract_bignum(a, b, c);
                         b->signbit = MINUS;
 		}
+		
 		return;
 	}
 
-	c->lastdigit = MAX(a->lastdigit,b->lastdigit)+1;
+	c->lastdigit = MAX(a->lastdigit, b->lastdigit) + 1;
 	carry = 0;
 
-	for (i=0; i<=(c->lastdigit); i++) {
-		c->digits[i] = (char) (carry+a->digits[i]+b->digits[i]) % 10;
+	for (i = 0; i <= (c->lastdigit); i++) 
+	{
+		c->digits[i] = (char) (carry + a->digits[i] + b->digits[i]) % 10;
 		carry = (carry + a->digits[i] + b->digits[i]) / 10;
 	}
 
@@ -137,15 +139,17 @@ void subtract_bignum(bignum *a, bignum *b, bignum *c)
 
 	initialize_bignum(c);
 
-	if ((a->signbit == MINUS) || (b->signbit == MINUS)) {
+	if ((a->signbit == MINUS) || (b->signbit == MINUS)) 
+	{
                 b->signbit = -1 * b->signbit;
-                add_bignum(a,b,c);
+                add_bignum(a, b, c);
                 b->signbit = -1 * b->signbit;
 		return;
         }
 
-	if (compare_bignum(a,b) == PLUS) {
-		subtract_bignum(b,a,c);
+	if (compare_bignum(a, b) == PLUS) 
+	{
+		subtract_bignum(b, a, c);
 		c->signbit = MINUS;
 		return;
 	}
@@ -153,11 +157,15 @@ void subtract_bignum(bignum *a, bignum *b, bignum *c)
         c->lastdigit = MAX(a->lastdigit,b->lastdigit);
         borrow = 0;
 
-        for (i=0; i<=(c->lastdigit); i++) {
+        for (i = 0; i <= (c->lastdigit); i++) 
+	{
 		v = (a->digits[i] - borrow - b->digits[i]);
 		if (a->digits[i] > 0)
+		{
 			borrow = 0;
-		if (v < 0) {
+		}
+		if (v < 0) 
+		{
 			v = v + 10;
 			borrow = 1;
 		}
@@ -173,15 +181,35 @@ int compare_bignum(bignum *a, bignum *b)
 {
 	int i;				/* counter */
 
-	if ((a->signbit == MINUS) && (b->signbit == PLUS)) return(PLUS);
-	if ((a->signbit == PLUS) && (b->signbit == MINUS)) return(MINUS);
+	if ((a->signbit == MINUS) && (b->signbit == PLUS))
+	{	
+		return(PLUS);
+	}
+	
+	if ((a->signbit == PLUS) && (b->signbit == MINUS))
+	{
+		return(MINUS);
+	}
 
-	if (b->lastdigit > a->lastdigit) return (PLUS * a->signbit);
-	if (a->lastdigit > b->lastdigit) return (MINUS * a->signbit);
+	if (b->lastdigit > a->lastdigit)
+	{	
+		return (PLUS * a->signbit);
+	}
+	if (a->lastdigit > b->lastdigit) 
+	{	
+		return (MINUS * a->signbit);
+	}
 
-	for (i = a->lastdigit; i>=0; i--) {
-		if (a->digits[i] > b->digits[i]) return(MINUS * a->signbit);
-		if (b->digits[i] > a->digits[i]) return(PLUS * a->signbit);
+	for (i = a->lastdigit; i>=0; i--) 
+	{
+		if (a->digits[i] > b->digits[i])
+		{	
+			return(MINUS * a->signbit);
+		}
+		if (b->digits[i] > a->digits[i])
+		{	
+			return(PLUS * a->signbit);
+		}
 	}
 
 	return(0);
@@ -190,10 +218,15 @@ int compare_bignum(bignum *a, bignum *b)
 void zero_justify(bignum *n)
 {
 	while ((n->lastdigit > 0) && (n->digits[ n->lastdigit ] == 0))
-		n->lastdigit --;
+	{
+		n->lastdigit--;
+	}
 
         if ((n->lastdigit == 0) && (n->digits[0] == 0))
+	{
 		n->signbit = PLUS;	/* hack to avoid -0 */
+	}
+	
 	return;
 }
 
@@ -202,12 +235,20 @@ void digit_shift(bignum *n, int d)		/* multiply n by 10^d */
 {
 	int i;				/* counter */
 
-	if ((n->lastdigit == 0) && (n->digits[0] == 0)) return;
+	if ((n->lastdigit == 0) && (n->digits[0] == 0))
+	{	
+		return;
+	}
 
-	for (i=n->lastdigit; i>=0; i--)
-		n->digits[i+d] = n->digits[i];
+	for (i = n->lastdigit; i >= 0; i--)
+	{
+		n->digits[i + d] = n->digits[i];
+	}
 
-	for (i=0; i<d; i++) n->digits[i] = 0;
+	for (i = 0; i < d; i++)
+	{	
+		n->digits[i] = 0;
+	}
 
 	n->lastdigit = n->lastdigit + d;
 	return;
@@ -219,18 +260,20 @@ void multiply_bignum(bignum *a, bignum *b, bignum *c)
 {
 	bignum row;			/* represent shifted row */
 	bignum tmp;			/* placeholder bignum */
-	int i,j;			/* counters */
+	int i, j;			/* counters */
 
 	initialize_bignum(c);
 
 	row = *a;
 
-	for (i=0; i<=b->lastdigit; i++) {
-		for (j=1; j<=b->digits[i]; j++) {
-			add_bignum(c,&row,&tmp);
+	for (i = 0; i <= b->lastdigit; i++) 
+	{
+		for (j=1; j <= b->digits[i]; j++) 
+		{
+			add_bignum(c, &row, &tmp);
 			*c = tmp;
 		}
-		digit_shift(&row,1);
+		digit_shift(&row, 1);
 	}
 
 	c->signbit = a->signbit * b->signbit;
@@ -244,8 +287,8 @@ void divide_bignum(bignum *a, bignum *b, bignum *c)
 {
         bignum row;                     /* represent shifted row */
         bignum tmp;                     /* placeholder bignum */
-		int asign, bsign;		/* temporary signs */
-        int i,j;                        /* counters */
+	int asign, bsign;		/* temporary signs */
+        int i, j;                       /* counters */
 
 	initialize_bignum(c);
 
@@ -262,13 +305,15 @@ void divide_bignum(bignum *a, bignum *b, bignum *c)
 
 	c->lastdigit = a->lastdigit;
 
-	for (i=a->lastdigit; i>=0; i--) {
-		digit_shift(&row,1);
+	for (i = a->lastdigit; i>=0; i--) 
+	{
+		digit_shift(&row, 1);
 		row.digits[0] = a->digits[i];
 		c->digits[i] = 0;
-		while (compare_bignum(&row,b) != PLUS) {
-			c->digits[i] ++;
-			subtract_bignum(&row,b,&tmp);
+		while (compare_bignum(&row, b) != PLUS) 
+		{
+			c->digits[i]++;
+			subtract_bignum(&row, b, &tmp);
 			row = tmp;
 		}
 	}
