c22e3459cb9f886a6d6fc827af933f7857bf434d
raiyanshadow <taseenraiyan@gmail.com>
2022-09-12 22:44:38 -0400

    c question 5 optimization

--- questions/c/EulQ05.c
+++ questions/c/EulQ05.c
@@ -1,7 +1,8 @@
 // Smallest multiple
 
-#include "essentials.c"
-
+// Took ~4 seconds before, but now it takes ~0.0 ms
+// Original:
+/*
 int main()
 {
     int divisibleMax;
@@ -43,4 +44,39 @@ int main()
 
     timeEnd();
     return 0;
+}
+*/
+
+#include "essentials.c"
+
+long long gcd(long long x1, long long x2);
+
+int main()
+{
+    int divisibleMax;
+    long long ret = 1;
+    scanf("%i", &divisibleMax);
+
+    timeStart();
+
+    for (int i = 2; i <= divisibleMax; i++)
+    {
+        ret = (ret * i)/gcd(ret, i);
+    }
+    printf("%lli\n", ret);
+
+    timeEnd();
+    return 0;
+}
+
+long long gcd(long long x1, long long x2)
+{
+    if (x2 != 0)
+    {
+        return gcd(x2, x1 % x2);
+    }
+    else
+    {
+        return x1;
+    }
 }
